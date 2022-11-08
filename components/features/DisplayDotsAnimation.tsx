8d8ad3dcfdd50c35e238bf986f9ac9183f16b5bc
Riviere Fougy <rivierefougy@gmail.com>
2022-11-08 17:13:59 -0500

    feat: creation of DisplayDotsAnimation file, adding comments

---
+++ components/features/DisplayDotsAnimation.tsx
@@ -0,0 +1,20 @@
+/**
+ * @description Providing methods and approach to use for DisplayDots animation.
+ * 
+ * @description using setInterval to have dots dissapear at given time intervals, clearInterval for stopping setInterval once the text is fully shown.
+ * @example Using setInterval and clearInterval
+ * var refreshId = setInterval(function() {
+ *      var properID = CheckReload();
+ *      if (properID > 0) {
+ *          <--- exit from the loop--->
+ *      }
+ *   }, 10000);
+ * 
+ * @note 1 second = 1000 milliseconds
+ */
+
+import React from "react";
+
+export const DisplayDotsAnimation = () => {
+  return <div>DisplayDotsAnimation</div>;
+};

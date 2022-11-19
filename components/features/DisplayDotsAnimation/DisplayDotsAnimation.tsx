11716ca51dc446dce707d92dfe197006eee60ed8
Riviere Fougy <rivierefougy@gmail.com>
2022-11-19 10:58:23 +0100

    feat: calculate start end x coordinates per character, refactoring

--- components/features/DisplayDotsAnimation/DisplayDotsAnimation.tsx
+++ components/features/DisplayDotsAnimation/DisplayDotsAnimation.tsx
@@ -1,62 +1,62 @@
 import styles from "./DisplayDotsAnimation.module.css";
 import { displayDotsDictionary } from "../../../dictionaries/displayDotsDictionary";
 import { useEffect } from "react";
+import "../../../ext/string.extensions";
 
 const DisplayDotsAnimation: React.FC<{ string?: string }> = ({
-  string = "EE E",
+  string = "EE EE",
 }) => {
   const wordsArr: string[] = string.toUpperCase().split(" "); // Step 1
 
-  let charStartingXCoordPosition: number = 0;
-  let maxXCoordForStr: number = 0;
-  let maxYCoordForStr: number = 0;
+  let charStartingXCoord: number = 0;
 
-  function getActiveDotCoords(charArr: string[]) {
-    const activeDotCoords = charArr.map((char: string, index: number) => {
+  function getActiveCoords(charArr: string[]) {
+    const dictionaryCharArr = charArr.map((char: string) => {
       const displayDotsDictionaryClone = structuredClone(displayDotsDictionary);
       const dictionaryChar = displayDotsDictionaryClone[char];
 
+      return dictionaryChar;
+    });
+
+    const activeCoords = dictionaryCharArr.map((dictionaryChar: any) => {
       let newCoords;
 
       if (dictionaryChar.coordinates !== null) {
         newCoords = structuredClone(dictionaryChar.coordinates);
-
         newCoords.map((coordinate: number[]) => {
-          coordinate[1] += charStartingXCoordPosition;
+          coordinate[1] += charStartingXCoord;
         });
 
         dictionaryChar.coordinates = newCoords;
       }
 
-      charStartingXCoordPosition +=
-        index === charArr.lastIndexOf(char)
-          ? dictionaryChar.width + 1 // Step 2a
-          : dictionaryChar.width;
+      charStartingXCoord += dictionaryChar.width;
 
       return dictionaryChar.coordinates;
     });
 
-    return activeDotCoords;
+    charStartingXCoord += 2; // adding empty space for next word.
+
+    return activeCoords;
   }
 
-  function getAllDotCoordinates(activeDotCoords: number[][]) {
-    const allDotCoords = [];
+  function getAllCoords(activeCoords: number[][]): number[][] {
+    const allCoords = [];
 
-    for (let i = 0; i < activeDotCoords.length; i++) {
-      const currCoord = activeDotCoords[i];
-      const nextCoord = activeDotCoords[i + 1];
-      const maxXCoord = activeDotCoords[activeDotCoords.length - 1][1];
+    for (let i = 0; i < activeCoords.length; i++) {
+      const currCoord = activeCoords[i];
+      const nextCoord = activeCoords[i + 1];
+      const maxXCoord = activeCoords[activeCoords.length - 1][1];
 
-      allDotCoords.push(currCoord);
+      allCoords.push(currCoord);
 
       if (
         nextCoord &&
         currCoord[0] === nextCoord[0] &&
         nextCoord[1] - currCoord[1] > 1
       ) {
-        const inactiveCoords = getGapCoords(currCoord, nextCoord[1]);
-        allDotCoords.push(...inactiveCoords);
-        console.log("MATCH");
+        const inactiveCoords = getGapCoordsHelper(currCoord, nextCoord[1]);
+        allCoords.push(...inactiveCoords);
       }
 
       if (
@@ -64,18 +64,15 @@ const DisplayDotsAnimation: React.FC<{ string?: string }> = ({
         currCoord[0] !== nextCoord[0] &&
         maxXCoord - currCoord[1] > 1
       ) {
-        console.log(
-          `${maxXCoord} minus ${currCoord[1]} = ${maxXCoord - currCoord[1]}`
-        );
-        const inactiveCoords = getGapCoords(currCoord, maxXCoord, true);
-        allDotCoords.push(...inactiveCoords);
+        const inactiveCoords = getGapCoordsHelper(currCoord, maxXCoord, true);
+        allCoords.push(...inactiveCoords);
       }
     }
 
-    return allDotCoords;
+    return allCoords;
   }
 
-  function getGapCoords(
+  function getGapCoordsHelper(
     currCoord: number[],
     largerXCoord: number,
     isMaxXCoord?: boolean
@@ -94,67 +91,83 @@ const DisplayDotsAnimation: React.FC<{ string?: string }> = ({
 
     return gapCoords;
   }
-  /**
-   * @todo Create custom builtin method to replace sorting method
-  */ 
-  const activeDotCoords = wordsArr
-    .reduce(
-      // Step 2
-      (output: any[], word: string) => {
-        const charArr: string[] = word.split("");
-        const activeDotCoordsForWord = getActiveDotCoords(charArr);
-        output.push(...activeDotCoordsForWord);
-        return output;
-      },
-      []
-    )
-    .flat(1) // Step 2b
-    .sort(function (a, b) {
-      if (a[0] == b[0]) {
-        return a[1] - b[1];
-      }
-      return a[0] - b[0];
+
+  function getStartAndEndXCoordsPerChar(): number[][] {
+    const charArr = string.split("");
+
+    let startXCoord: number = 0;
+    let endXCoord: number;
+    let startEndXCoord: number[];
+
+    const startEndXCoordsPerChar = charArr.map((char): number[] => {
+      const displayDotsDictionaryClone = structuredClone(displayDotsDictionary);
+      const dictionaryChar = displayDotsDictionaryClone[char];
+      const dictionaryCharWidth = isWhiteSpaceHelper(char)
+        ? dictionaryChar.width
+        : dictionaryChar.width - 1;
+
+      endXCoord = startXCoord + dictionaryCharWidth;
+      startEndXCoord = [startXCoord, endXCoord];
+      startXCoord = startXCoord + dictionaryCharWidth + 1;
+
+      return startEndXCoord;
     });
 
-  const allDotCoords = getAllDotCoordinates(activeDotCoords).sort(function (
-    a,
-    b
-  ) {
-    if (a[0] == b[0]) {
-      return a[1] - b[1];
-    }
-    return a[0] - b[0];
-  });
+    return startEndXCoordsPerChar;
+  }
 
-  const inactiveDotCoords = allDotCoords
-    .filter((coord) => !activeDotCoords.includes(coord))
-    .sort(function (a, b) {
-      if (a[0] == b[0]) {
-        return a[1] - b[1];
-      }
-      return a[0] - b[0];
-    });
+  function isWhiteSpaceHelper(char: string) {
+    return /\s/.test(char);
+  }
+
+  const activeCoordsTEST: number[][][] = wordsArr.reduce(
+    // Step 2
+    (output: any[], word: string, indexOfWordInArr) => {
+      const charArr: string[] = word.split("");
+      const activeCoordsForWord = getActiveCoords(charArr);
+      output.push(...activeCoordsForWord);
+      return output;
+    },
+    []
+  );
+
+  const activeCoords: number[][] = activeCoordsTEST.flat(1).sortCoords();
+
+  // includes coordinates for spaces
+  const allCoords: number[][] = getAllCoords(activeCoords).sortCoords();
+
+  const inactiveCoords: number[][] = allCoords
+    .filter((coord) => !activeCoords.includes(coord))
+    .sortCoords();
 
   useEffect(() => {
-    console.log("activeDotCoords: ", activeDotCoords);
-    console.log("allDotCoords: ", allDotCoords);
-    console.log("inactiveDotCoords: ", inactiveDotCoords);
+    console.log("activeCoordsTEST: ", activeCoordsTEST);
+    console.log(
+      "getStartAndEndXCoordsPerChar: ",
+      getStartAndEndXCoordsPerChar()
+    );
+
+    console.log("activeCoords: ", activeCoords);
+    console.log("allCoords: ", allCoords);
+    console.log("inactiveCoords: ", inactiveCoords);
   });
 
   /**
    *
    * declare a variable where the value is a filter through the above variable. The result is an array of inactive coordinates, aka negative space.
    *
+   * *********CREATE TESTS
+   *
    * Need to take into account words and flex wrapping...
    * [X] 1. split the string into an array of words
    * [X] 2. map through each word and get active dot coordinates;
-   * [X]   a. add 1 extra width to totalWidth (aka charStartingXCoordPosition) to account for missing spaces inbetween words.
+   * [X]   a. add 1 extra width to totalWidth (aka charStartingXCoord) to account for missing spaces inbetween words.
    * [X]   b. after mapping, flatten array (Ex. [[[0,1], [0,2]], [[1,1], [2,2]]] --> [[0,1], [0,2], [1,1], [2,2]]).
    * [X] 3. create list of inactive dot coordinates that references the flattened array.
    * [X] 4. create list 'mergedList' that merges both active and inactive coordinate lists.
    *    a. the general idea is to reference coordinates in mergedList, then group them by width
    *      i. Ex. word has max xcoord of 4, group coordinates with max xcoord 4).
-   *    b. after/while grouping, sort coordinates in each group in ascending order (Ex. [0,0] [0,1] [0,2] [1,0] ...)
+   *    b. after/while grouping, sort coordinates in each group in ascending order (Ex. [[0,0] [0,1] [0,2]] [[1,0]] ...)
    *      i. *Note: is there a built-in method that can sort mergedList?
    *
    *  5. From here we would create the HTML structure.

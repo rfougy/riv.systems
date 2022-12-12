dd01b53916b1c20211d576ecb916f0d0f10f82ea
Riviere Fougy <77861258+rfougy@users.noreply.github.com>
2022-12-11 22:19:34 -0500

    ┐(´ー｀)┌ fix: pagination border color

--- components/features/pagination/Pagination.styled.ts
+++ components/features/pagination/Pagination.styled.ts
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 export const Container = styled.div`
   margin-top: 4rem;
-  border: solid;
+  border: solid ${(props: any) => props.theme.primary};
   max-width: 15rem;
   margin: 4rem auto 0 auto;
   padding: 0.25rem;

3ebaa12829247cff24c0d044360531a198722c87
Riviere Fougy <rivierefougy@gmail.com>
2023-01-06 12:00:44 -0500

    refactor: open graph to _document for test purposes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,20 @@ class MyDocument extends Document {
         <Head>
           <Favicon />
           <PwaHead />
+          <title>RIV.SYSTEMS</title>
+          <meta name="description" content="This is a test test description" />
+
+          {/* Social Share via Open Graph */}
+          <meta property="og:site_name" content="RIV.SYSTEMS" />
+          <meta property="og:title" content="RIV.SYSTEMS" />
+          <meta
+            property="og:description"
+            content="This is a test description"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="https://i.imgur.com/dk7mgAz.png" />
+          <meta property="og:url" content="https://riv.systems" />
+          <meta name="twitter:card" content="summary_large_image" />
         </Head>
         <body>
           <Main />

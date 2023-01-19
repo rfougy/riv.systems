8b5531494882bfe1b923553b3bbefce0d863223d
Riviere Fougy <77861258+rfougy@users.noreply.github.com>
2023-01-18 21:38:22 -0500

    feat: custom 404 and 500 error pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,6 +30,10 @@ const App = (props: any) => {
     useState<boolean>(true);
   const [hydrated, setHydrated] = useState<boolean>(false);
 
+  // Home Page, 404 Page, 500 Page
+  // @ts-ignore
+  const isDisplayDotsPage = pageProps.isDisplayDotsPage ? true : false;
+
   const announcement: IAnnouncement = {
     dateCreated: "2022-01-06",
     text: "'Custom Biker Jacket References' post is now online.",
@@ -130,7 +134,6 @@ const App = (props: any) => {
 
   return (
     <>
-      {/* description: references metaTagInputs via pre-rendering methods */}
       {/* @ts-ignore */}
       <PageHead {...pageProps.metaTagInputs} />
       <PwaHead />
@@ -146,7 +149,7 @@ const App = (props: any) => {
         )}
         <PageContainer>
           <Navbar toggleTheme={toggleTheme} theme={currTheme} />
-          <ContentWrap isHomePage={route === "/"}>
+          <ContentWrap isDisplayDotsPage={isDisplayDotsPage}>
             <Component {...pageProps} />
           </ContentWrap>
           <Footer />
---
+++ styles/pages/404.styled.ts
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Description = styled.p`
+  text-align: center;
+`;
+
+export const Margin = styled.div`
+  margin: 2rem 0;
+`;
+
+export const Button = styled.button`
+  border: 0.1rem solid ${(props: any) => props.theme.primary};
+  border-radius: 5vh;
+
+  padding: 0.5rem 1rem;
+  margin-top: 1rem;
+  color: ${(props: any) => props.theme.primary};
+  background-color: ${(props: any) =>
+    props.theme.id === "light" ? props.theme.secondary : "none"};
+
+  &:hover {
+    color: ${(props: any) =>
+      props.theme.id === "light" ? props.theme.secondary : "none"};
+    background-color: ${(props: any) =>
+      props.theme.id === "light" ? props.theme.primary : "#2D2D2D"};
+  }
+`;

50b25a21f79ef7637a2e0d78c62bf5d6a6c70e6f
Riviere Fougy <77861258+rfougy@users.noreply.github.com>
2023-02-06 10:32:07 -0500

    style: divider for post page header (#100)

--- components/posts/page/PostPage.styled.ts
+++ components/posts/page/PostPage.styled.ts
@@ -1,31 +1,28 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  max-width: 50rem;
-`;
-
-export const Metadata = styled.ul`
-  list-style-type: none;
-  margin-bottom: 1rem;
-  padding: 0;
-`;
-
 export const Header = styled.header``;
 
 export const Title = styled.h1`
   font-size: 2rem;
-  margin: 0;
+  margin: 1rem 0 1.75rem 0;
 
   @media (max-width: 40em) {
     font-size: 1.75rem;
   }
 `;
 
-export const CoverImage = styled.div`
-  margin-top: 1.5rem;
+export const Excerpt = styled.p`
+  margin: 0;
+`;
+
+export const Metadata = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`;
+
+export const Inline = styled.li`
+  display: inline-block;
+  margin-right: 0.5rem;
 `;
 
 export const Li = styled.li`
@@ -35,13 +32,17 @@ export const Li = styled.li`
   }
 `;
 
-export const Inline = styled.li`
-  display: inline-block;
-  margin-right: 0.5rem;
+export const Divider = styled.div`
+  height: 1px;
+  background: ${(props: any) => props.theme.primary};
+  margin: 2rem 0 3.5rem 0;
+  opacity: 0.5;
 `;
 
+export const CoverImage = styled.div``;
+
 export const Content = styled.div`
-  margin-top: 3rem;
+  margin-top: 2.125rem;
 
   a {
     font-size: 0.875rem;
@@ -67,4 +68,11 @@ export const Content = styled.div`
   ul img {
     margin: 0.25rem 0;
   }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 50rem;
 `;

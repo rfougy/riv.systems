afda948c914851982ffd1944c6eee5a35ca35ce9
Riviere Fougy <rivierefougy@gmail.com>
2023-02-08 09:55:39 -0500

    refactor(filter-menu): reverse sort of sections

--- utils/sortInAlphabeticOrder.ts
+++ utils/sortInAlphabeticOrder.ts
@@ -1,5 +1,5 @@
 export function sortInAlphabeticOrder(a: string, b: string): number {
   const textA: string = a.toLowerCase();
   const textB: string = b.toLowerCase();
-  return textA > textB ? 1 : -1;
+  return textA > textB ? -1 : 1;
 }

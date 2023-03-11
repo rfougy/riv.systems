25a70a5998edfe254b794a52ead8ebaa77da126b
Riviere Fougy <rivierefougy@gmail.com>
2023-03-11 10:48:36 -0500

    feat(AudioPlayer): basic next and prev skip functionality

--- components/features/audio-player/AudioPlayer.tsx
+++ components/features/audio-player/AudioPlayer.tsx
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Howl } from "howler";
 
 import AudioSample01 from "../../../public/audio/music/audio-player-sample-1.mp3";
 import AudioSample02 from "../../../public/audio/music/audio-player-sample-2.mp3";
 import { Container } from "./AudioPlayer.styled";
 
+const playlist = [AudioSample01, AudioSample02];
+
 export const AudioPlayer: React.FC = () => {
-  const howler = new Howl({
-    src: [AudioSample01, AudioSample02],
-  });
+  const [currSongIndex, setCurrSongIndex] = useState<number>(0);
+  const [howler, setHowler] = useState<Howl>(
+    initializeHowler(playlist[currSongIndex])
+  );
+
+  function initializeHowler(song: string): Howl {
+    const howler = new Howl({
+      src: [song],
+    });
+
+    return howler;
+  }
+
+  function handleNextSong() {
+    howler.pause();
+    setCurrSongIndex(currSongIndex + 1);
+  }
+
+  function handlePrevSong() {
+    howler.pause();
+    setCurrSongIndex(currSongIndex - 1);
+  }
+
+  useEffect(() => {
+    const newHowler = initializeHowler(playlist[currSongIndex]);
+    setHowler(newHowler);
+  }, [currSongIndex]);
 
   return (
     <Container>
-      <div
-        onClick={() => {
-          console.log("PLAYING");
-          howler.play();
-        }}
-      >
-        PLAY
-      </div>
-      <div
-        onClick={() => {
-          console.log("PAUSED");
-          howler.pause();
-        }}
-      >
-        PAUSE
-      </div>
+      <div onClick={() => howler.play()}>PLAY</div>
+      <div onClick={() => howler.pause()}>PAUSE</div>
+      <div onClick={() => handleNextSong()}>SKIP FORWARD</div>
+      <div onClick={() => handlePrevSong()}>SKIP BACKWARD</div>
     </Container>
   );
 };

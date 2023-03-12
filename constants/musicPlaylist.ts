fb5615483e00195e3b4e7a8b02c552db6a2704f4
Riviere Fougy <rivierefougy@gmail.com>
2023-03-11 22:51:46 -0500

    refactor: replace audio files with compressed versions

--- constants/musicPlaylist.ts
+++ constants/musicPlaylist.ts
@@ -3,8 +3,7 @@ import track2 from "../public/audio/music/Ivy-Lab_Balaclava.mp3";
 import track3 from "../public/audio/music/Ivy-Lab_Candle-Song.mp3";
 import track4 from "../public/audio/music/Lapalux_4EVA_ft.-Talvi.mp3";
 import track5 from "../public/audio/music/Moore-Kismet_Rumor-ft.-WYN.mp3";
-import track6 from "../public/audio/music/The-Glitch-Mob_All-The-People.mp3";
-import track7 from "../public/audio/music/The-Glitch-Mob_E-Motion.mp3";
+import track6 from "../public/audio/music/The-Glitch-Mob_E-Motion.mp3";
 import { shuffleArr } from "../utils";
 
 export const musicPlaylist: string[] = shuffleArr([
@@ -14,5 +13,4 @@ export const musicPlaylist: string[] = shuffleArr([
   track4,
   track5,
   track6,
-  track7,
 ]);

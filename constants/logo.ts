269fe79f62dda108f39b736dd42cbe5689bd6d09
Riviere Fougy <rivierefougy@gmail.com>
2023-03-24 21:44:25 -0400

    refactor: remove unused logo files

--- constants/logo.ts
+++ constants/logo.ts
@@ -1,19 +1,9 @@
-import logoActiveLight from "../public/assets/logo-active-light.svg";
-import logoHiddenLight from "../public/assets/logo-hidden-light.svg";
-import logoActiveDark from "../public/assets/logo-active-dark.svg";
-import logoHiddenDark from "../public/assets/logo-hidden-dark.svg";
+import logoLongActive from "../public/assets/logo/logo-long-active.svg";
+import logoLongHidden from "../public/assets/logo/logo-long-hidden.svg";
 
 export const logo: any = {
-  light: {
-    active: logoActiveDark,
-    hidden: logoHiddenDark,
-  },
-  dark: {
-    active: logoActiveLight,
-    hidden: logoHiddenLight,
-  },
   long: {
-    active: logoActiveDark,
-    hidden: logoHiddenDark,
+    active: logoLongActive,
+    hidden: logoLongHidden,
   },
 };

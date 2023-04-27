d899980aee1993cd9eada8f89509507be20656eb
Riviere Fougy <rivierefougy@gmail.com>
2023-04-27 05:20:12 -0400

    feat: add link to source code to footer

---
+++ constants/socialsList.ts
@@ -0,0 +1,20 @@
+import ISocials from "../interfaces/ISocials";
+
+export const socialsList: ISocials[] = [
+  {
+    title: "GitHub",
+    url: "https://github.com/rfougy",
+  },
+  {
+    title: "LinkedIn",
+    url: "https://www.linkedin.com/in/riviere-fougy/",
+  },
+  {
+    title: "Instagram",
+    url: "https://www.instagram.com/riv.ig/",
+  },
+  {
+    title: "Email",
+    url: "mailto:rivierefougy@gmail.com",
+  },
+];

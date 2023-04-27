bb7f38f9cb25b037c9fe97d8dff6e7f58bdb2cc2
Riviere Fougy <77861258+rfougy@users.noreply.github.com>
2023-04-27 05:25:22 -0400

    Feat: Add Source Code Link to Footer (#124)
    * feat: add link to source code to footer
    
    * fix(Footer): change list to flex & add gap

--- components/footer/Footer.styled.ts
+++ components/footer/Footer.styled.ts
@@ -3,22 +3,33 @@ import styled from "@emotion/styled";
 
 export const Footer = styled.footer`
   display: flex;
-  flex-direction: column;
   height: 1.375rem
-  justify-content: end;
+  width: 100%;
+  justify-content: space-between;
+
+  @media (max-width: ${breakpoints.xxs}) {
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.5rem;
+  }
 `;
 
 export const SocialsList = styled.ul`
   list-style-type: none;
-  margin: 0 0 0 auto;
+
+  display: flex;
+  gap: 1.5rem;
   padding: 0;
 
-  @media (max-width: ${breakpoints.xs}) {
-    margin: 0 auto;
+  @media (max-width: ${breakpoints.xxs}) {
+    margin: auto;
   }
 `;
 
 export const SocialsLink = styled.li`
   display: inline-block;
-  padding: 0 0.75rem;
+
+  @media (max-width: ${breakpoints.xxs}) {
+    margin: auto;
+  }
 `;
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,31 +1,22 @@
+import { socialsList } from "../../constants/socialsList";
 import ISocials from "../../interfaces/ISocials";
 
 import { Footer as Foot, SocialsList, SocialsLink } from "./Footer.styled";
 
-const socials: ISocials[] = [
-  {
-    title: "GitHub",
-    url: "https://github.com/rfougy",
-  },
-  {
-    title: "LinkedIn",
-    url: "https://www.linkedin.com/in/riviere-fougy/",
-  },
-  {
-    title: "Instagram",
-    url: "https://www.instagram.com/riv.ig/",
-  },
-  {
-    title: "Email",
-    url: "mailto:rivierefougy@gmail.com",
-  },
-];
-
 const Footer: React.FC = () => {
   return (
     <Foot>
+      <SocialsLink>
+        <a
+          href={"https://github.com/rfougy/riv.systems"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Source Code
+        </a>
+      </SocialsLink>
       <SocialsList>
-        {socials.map((social: ISocials, index: number) => (
+        {socialsList.map((social: ISocials, index: number) => (
           <SocialsLink key={index}>
             <a href={social.url} target="_blank" rel="noreferrer">
               {social.title}

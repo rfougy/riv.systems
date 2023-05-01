b38778149dcceeafc84872353c0e46a6181c5146
Riviere Fougy <rivierefougy@gmail.com>
2023-05-01 16:36:13 +0200

    refactor(additional.d.ts): re-add module declarations

--- additional.d.ts
+++ additional.d.ts
@@ -6,3 +6,13 @@ declare module "*.mp3" {
   const value: string;
   export default value;
 }
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}

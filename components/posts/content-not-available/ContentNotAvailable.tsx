7edc591b92bb81e108b1c596f8570b022888057d
Riviere Fougy <rivierefougy@gmail.com>
2023-05-27 13:55:27 -0400

    fix: NotFound component for ContentNotAvailable

--- components/posts/content-not-available/ContentNotAvailable.tsx
+++ components/posts/content-not-available/ContentNotAvailable.tsx
@@ -1,35 +1,11 @@
 import React from "react";
-import Link from "next/link";
+import NotFound from "../../shared/not-found/NotFound";
 
-import DisplayDotsCoordsProvider from "../../context/DisplayDotsCoordsContext";
-
-import DisplayDotsAnime from "../../features/display-dots-anime/DisplayDotsAnime";
-
-import {
-  Button,
-  Container,
-  Description,
-  Margin,
-} from "../../../styles/pages/DisplayDotsPage.styled";
-
-const ContentNotAvailable = () => {
-  return (
-    <>
-      <DisplayDotsCoordsProvider>
-        <Container>
-          <Margin>
-            <DisplayDotsAnime text={"NO CONTENT"} />
-          </Margin>
-          <Description>
-            The content you&apos;re searching for cannot be found.
-          </Description>
-          <Link href={`/content`} passHref>
-            <Button>RETURN TO SYSTEMS</Button>
-          </Link>
-        </Container>
-      </DisplayDotsCoordsProvider>
-    </>
-  );
-};
+const ContentNotAvailable: React.FC = () => (
+  <NotFound
+    title="NO CONTENT"
+    copy="The content you're searching for cannot be found."
+  />
+);
 
 export default ContentNotAvailable;
--- components/shared/not-found/NotFound.tsx
+++ components/shared/not-found/NotFound.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+import Link from "next/link";
+
+import DisplayDotsCoordsProvider from "../../context/DisplayDotsCoordsContext";
+
+import DisplayDotsAnime from "../../features/display-dots-anime/DisplayDotsAnime";
+
+import {
+  Button,
+  Container,
+  Description,
+  Margin,
+} from "../../../styles/pages/DisplayDotsPage.styled";
 
 const NotFound: React.FC<{ title: string; copy: string }> = ({
   title,
   copy,
-}) => <NotFound title={title} copy={copy} />;
+}) => (
+  <>
+    <DisplayDotsCoordsProvider>
+      <Container>
+        <Margin>
+          <DisplayDotsAnime text={title} />
+        </Margin>
+        <Description>{copy}</Description>
+        <Link href={`/content`} passHref>
+          <Button>RETURN TO SYSTEMS</Button>
+        </Link>
+      </Container>
+    </DisplayDotsCoordsProvider>
+  </>
+);
 
 export default NotFound;

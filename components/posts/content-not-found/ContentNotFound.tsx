ad7bbe362e43b0d92172c6bc97ff750a1d1af9d2
Riviere Fougy <rivierefougy@gmail.com>
2023-05-27 14:01:09 -0400

    refactor: change ContentNotAvailable to ContentNotFound

--- components/posts/content-not-found/ContentNotFound.tsx
+++ components/posts/content-not-found/ContentNotFound.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import NotFound from "../../shared/not-found/NotFound";
 
-const ContentNotAvailable: React.FC = () => (
+const ContentNotFound: React.FC = () => (
   <NotFound
     title="NO CONTENT"
     copy="The content you're searching for cannot be found."
   />
 );
 
-export default ContentNotAvailable;
+export default ContentNotFound;

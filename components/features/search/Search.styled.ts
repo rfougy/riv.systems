4f4e08399b285a33d3c7513eb726f9ccecf488fc
Riviere Fougy <rivierefougy@gmail.com>
2023-05-29 17:59:15 -0400

    style: search bar width 100%

--- components/features/search/Search.styled.ts
+++ components/features/search/Search.styled.ts
@@ -3,7 +3,11 @@ import styled from "@emotion/styled";
 export const Input = styled.input`
   all: unset;
   cursor: cell;
+
   color: ${(props: any) => props.theme.primary};
+  width: 100%;
 `;
 
-export const Form = styled.form``;
+export const Form = styled.form`
+  width: 90%;
+`;

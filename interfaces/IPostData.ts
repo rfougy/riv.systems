38545af6771aefd509eb80f3a30be87e5da1ab3c
Riviere Fougy <rivierefougy@gmail.com>
2023-07-05 21:27:12 -0400

    fix(IPostData): frontmatter as IFrontMatter

--- interfaces/IPostData.ts
+++ interfaces/IPostData.ts
@@ -1,19 +1,9 @@
+import { IPostFrontMatter } from "./IPostFrontMatter";
+
 export default interface IPostData {
   fileName: string;
   section: string;
   category: string;
-  frontmatter: {
-    title: string;
-    datePublished: string;
-    section: string;
-    category: string;
-    excerpt: string;
-    coverImage: string;
-    link: string;
-    worksTeamSize: number;
-    worksRoles: string[];
-    worksDuration: string[];
-    worksTools: string[];
-  };
+  frontmatter: IPostFrontMatter;
   path: string;
 }

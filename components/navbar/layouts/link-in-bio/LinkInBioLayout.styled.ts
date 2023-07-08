774d62734fe141fe3373067d0bb3779af976b799
Riviere Fougy <rivierefougy@gmail.com>
2023-07-07 22:53:41 -0400

    stylle(LinkInBioLayout): hover styling for ArrowIcon

--- components/navbar/layouts/link-in-bio/LinkInBioLayout.styled.ts
+++ components/navbar/layouts/link-in-bio/LinkInBioLayout.styled.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { ArrowIcon } from "../../../icons/ArrowIcon.styled";
 
 export const Button = styled.button`
   border: 0.1rem solid ${(props: any) => props.theme.primary};
@@ -24,5 +25,16 @@ export const Button = styled.button`
       props.theme.id === "light" ? props.theme.secondary : "none"};
     background-color: ${(props: any) =>
       props.theme.id === "light" ? props.theme.primary : "#2D2D2D"};
+
+    ${ArrowIcon} {
+      border: solid
+        ${(props: any) =>
+          props.theme.id === "light" ? props.theme.secondary : ""};
+      border-width: 0 0.075rem 0.075rem 0;
+      padding: 0.125rem;
+
+      transform: rotate(-45deg);
+      -webkit-transform: rotate(-45deg);
+    }
   }
 `;

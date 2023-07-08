933ea3b0dd8ffe477febcb29462cfd7761c4aeff
Riviere Fougy <rivierefougy@gmail.com>
2023-07-07 23:55:50 -0400

    refactor(FeaturedPosts): html semantics

--- components/link-in-bio/featured-posts/FeaturedPosts.styled.ts
+++ components/link-in-bio/featured-posts/FeaturedPosts.styled.ts
@@ -1,12 +1,14 @@
 import styled from "@emotion/styled";
 
-export const List = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 2rem 0;
-
+export const Container = styled.section`
   padding: 2rem;
 
   border: solid ${(props: any) => props.theme.primary};
   border-radius: 2.5rem;
+`;
+
+export const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem 0;
 `;
--- components/link-in-bio/featured-posts/FeaturedPosts.tsx
+++ components/link-in-bio/featured-posts/FeaturedPosts.tsx
@@ -1,16 +1,18 @@
 import IPostData from "../../../interfaces/IPostData";
 import PostCard from "./card/PostCard";
 
-import { List } from "./FeaturedPosts.styled";
+import { Container, List } from "./FeaturedPosts.styled";
 
 const FeaturedPosts: React.FC<{
   posts: IPostData[];
 }> = ({ posts }) => (
-  <List>
-    {posts.map((post) => (
-      <PostCard key={post.path} post={post} />
-    ))}
-  </List>
+  <Container>
+    <List>
+      {posts.map((post) => (
+        <PostCard key={post.path} post={post} />
+      ))}
+    </List>
+  </Container>
 );
 
 export default FeaturedPosts;

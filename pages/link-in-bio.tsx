410d7fe3c0d6b13d119d6046bf7627c321f2f41e
Riviere Fougy <rivierefougy@gmail.com>
2023-07-09 08:22:07 -0400

    feat: install vanta and THREE, ui tests via link-in-bio

--- pages/link-in-bio.tsx
+++ pages/link-in-bio.tsx
@@ -14,20 +14,61 @@ import { includePlaceholderImage } from "../lib/dynamic-pages/includePlaceholder
 import { Container, Header } from "../styles/pages/LinkInBio.styled";
 import SocialsList from "../components/socials/socials-list/SocialsList";
 import { socialsList } from "../constants/socialsList";
+import { useEffect, useRef, useState } from "react";
+
+import NET from "vanta/dist/vanta.net.min";
+import * as THREE from "three";
 
 const LinkInBio: NextPage<{ featuredPosts: IPostData[] }> = ({
   featuredPosts,
-}) => (
-  <DisplayDotsCoordsProvider>
-    <Container>
-      <Header>
-        <DisplayDotsAnime text={"RIV.LINKS"} />
-        <SocialsList socials={socialsList} />
-      </Header>
-      <FeaturedPosts posts={featuredPosts} />
-    </Container>
-  </DisplayDotsCoordsProvider>
-);
+}) => {
+  const [vantaEffect, setVantaEffect] = useState<any>(null);
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    const threeScript = document.createElement("script");
+    threeScript.setAttribute("id", "threeScript");
+    threeScript.setAttribute(
+      "src",
+      "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
+    );
+    document.getElementsByTagName("head")[0].appendChild(threeScript);
+    return () => {
+      if (threeScript) {
+        threeScript.remove();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: vantaRef.current,
+          THREE,
+          color: 0x14b679,
+          backgroundColor: 0x15173c,
+          maxDistance: 34.0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destory();
+    };
+  }, [vantaEffect]);
+
+  return (
+    <DisplayDotsCoordsProvider>
+      <Container ref={vantaRef}>
+        <Header>
+          <DisplayDotsAnime text={"RIV.LINKS"} />
+          <SocialsList socials={socialsList} />
+        </Header>
+        <FeaturedPosts posts={featuredPosts} />
+      </Container>
+    </DisplayDotsCoordsProvider>
+  );
+};
 
 export default LinkInBio;
 

70e6e4cdc217b9879c284e025caa6232d7f8e6f1
Riviere Fougy <rivierefougy@gmail.com>
2023-09-29 19:32:49 -0400

    refactor: remove shuffle from playlists, change song orders

--- constants/audio-player/japaneseRockPlaylist.ts
+++ constants/audio-player/japaneseRockPlaylist.ts
@@ -1,27 +1,6 @@
 import { ISong } from "../../interfaces/audio-player/ISong";
-import { shuffleArr } from "../../utils/common/shuffleArr";
 
-export const japaneseRockPlaylist: ISong[] = shuffleArr([
-  {
-    title: "FightSong",
-    artist: "Eve",
-    src: "https://github.com/rfougy/riv.systems/assets/77861258/de20bfe2-8d73-4d39-b7f0-65a4d3183b9f",
-  },
-  {
-    title: "Kaikai Kitan",
-    artist: "Eve",
-    src: "https://github.com/rfougy/riv.systems/assets/77861258/9b73c477-3566-45eb-878c-e0ddf75f3b59",
-  },
-  {
-    title: "グッドバイ",
-    artist: "Toe",
-    src: "https://github.com/rfougy/riv.systems/assets/77861258/274ba791-04ab-44d9-842e-71fe56e55b35",
-  },
-  {
-    title: "first death",
-    artist: "TK from Ling tosite sigure",
-    src: "https://github.com/rfougy/riv.systems/assets/77861258/61ea6b5a-060c-4e25-bb4f-c6c15791901a",
-  },
+export const japaneseRockPlaylist: ISong[] = [
   {
     title: "VIVID VICE",
     artist: "Who-ya Extended",
@@ -37,4 +16,24 @@ export const japaneseRockPlaylist: ISong[] = shuffleArr([
     artist: "Ling tosite sigure",
     src: "https://github.com/rfougy/riv.systems/assets/77861258/2ff5e85b-2b67-4a0c-a20b-d23a9f66f908",
   },
-]);
+  {
+    title: "Kaikai Kitan",
+    artist: "Eve",
+    src: "https://github.com/rfougy/riv.systems/assets/77861258/9b73c477-3566-45eb-878c-e0ddf75f3b59",
+  },
+  {
+    title: "FightSong",
+    artist: "Eve",
+    src: "https://github.com/rfougy/riv.systems/assets/77861258/de20bfe2-8d73-4d39-b7f0-65a4d3183b9f",
+  },
+  {
+    title: "first death",
+    artist: "TK from Ling tosite sigure",
+    src: "https://github.com/rfougy/riv.systems/assets/77861258/61ea6b5a-060c-4e25-bb4f-c6c15791901a",
+  },
+  {
+    title: "グッドバイ",
+    artist: "Toe",
+    src: "https://github.com/rfougy/riv.systems/assets/77861258/274ba791-04ab-44d9-842e-71fe56e55b35",
+  },
+];
